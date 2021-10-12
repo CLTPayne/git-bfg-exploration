14c5d259ab4ad3836571a9e94afa9c9ba6ef5bec
Charlotte Payne <charlotte.payne@ft.com>
2021-10-12 11:17:41 +0100

    Adding a secret once

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
+const secret = "***REMOVED***"
+
 ReactDOM.render(
-    <App />, 
+    <App secret={secret} />, 
     document.getElementById('root')
 );
