d5a3be5f35e9eff5cd4349b415ad14f9d7b15a55
Charlotte Payne <charlotte.payne@ft.com>
2021-10-12 11:24:20 +0100

    Moving secrets around

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-const secret = "COOL_SECRET"
+const Secret = () => {
+    const newSecret = "COOL_SECRET"
+    return (
+        <Fragment>
+            <p>secrets are snazzy</p>
+            <p>like this one: {newSecret}</p>
+        </Fragment>
+    )
+}
 
 const App = () => (
     <div>
         Hello we have secrets:
-        <p>COOL_SECRET & {secret}</p>
+        <p>COOL_SECRET</p>
+        <p>moving stuff around but still having secrets</p>
+        <Secret />
     </div>
 )
 
